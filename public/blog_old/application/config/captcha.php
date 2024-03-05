2f0f4f549bd8d500627e7dab3e361e1457d251c8
mihaiionita84 <mihaiionita84@gmail.com>
2024-03-05 11:56:04 +0200

    ..

--- public/blog_old/application/config/captcha.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['config'] = array(
-    'img_path' => MAINSITE_STYLE_PATH . 'images/captcha/',
-    'img_url' => base_url() . MAINSITE_STYLE_PATH . 'images/captcha/',
-    'font_path' => MAINSITE_STYLE_PATH . 'images/ROCKI.TTF',
-    'img_width' => 140,
-    'img_height' => 30,
-);
